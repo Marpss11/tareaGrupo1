c1d67c4e6e2584300541b1467bac12b35655e992
Marpss11 <mariamparedes23@gmail.com>
2025-02-18 19:48:02 +0100

    create program about data types

---
+++ tarea_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    std::string name = "Juan";
+    int age = 6;
+    std::string address = "Santander, Cale Albert Einstein";
+    char firstLetter = 'R';
+
+    std::cout<<"Nombre:"<<name<<std::endl;
+    std::cout<<"Edad:"<<age<<std::endl;
+    std::cout<<"Direccion:"<<address<<std::endl;
+    std::cout<<"Primera letra del apellido:"<<firstLetter<<std::endl;
+    
+    return 0;
+}
